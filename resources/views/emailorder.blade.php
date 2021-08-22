433e799ace3f4d24a5286964631896ab4a461e5a
PramestiDyah67 <PramestiDyah15@student.uns.ac.id>
2021-08-22 21:15:38 +0700

    halaman email

--- resources/views/emailorder.blade.php
+++ resources/views/emailorder.blade.php
@@ -4,10 +4,6 @@
 Halo Translator SIKOMBASA!<br>
 Ada pesanan baru! Ayo segera buka akunmu dan ambil pesanan berikut.
 
-@component('mail::button', ['url' => $data['url']])
-Visit
-@endcomponent
-
 Thanks,<br>
 SIKOMBASA Team
 @endcomponent
